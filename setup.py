0c56ec0ad7a6508fcd01baea1d8eac9c5630d3a7
Luke Gregor <lukegre@gmail.com>
2018-06-14 17:02:45 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='vindta_reCAlk',
+      version='0.2',
+      date='2018-06-14',
+      description='Recalculates DIC and Alkalinity to CRM values from a VINDTA 3C.',
+      url='https://gitlab.com/socco/vindta_reCAlk',
+      author='Luke Gregor',
+      organisation='Council for Scientific and Industrial Research',
+      author_email='lukegre@gmail.com',
+      license='MIT',
+      packages=['vindta_reCAlk'],
+      zip_safe=False)
---
+++ vindta_reCAlk/__init__.py
@@ -0,0 +1,3 @@
+from .VINDTA_recALK import dbs_to_excel
+from .VINDTA_recALK import recalculate_CO2_from_excel
+from . import VINDTA_recALK as core
